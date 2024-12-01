50ede3c686b44771a5b5b706bc071caa3741a316
Yasaswini9000 <161327553+Yasaswini9000@users.noreply.github.com>
2024-12-01 10:34:49 +0530

    Create  Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Header.scss";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <h1>My React Task</h1>
+    </header>
+  );
+};
+
+export default Header;
