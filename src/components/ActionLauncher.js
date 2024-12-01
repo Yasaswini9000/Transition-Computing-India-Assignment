61de2f10660cadf85c73f0dd689c880f351f19ae
Yasaswini9000 <161327553+Yasaswini9000@users.noreply.github.com>
2024-12-01 11:03:15 +0530

    Create ActionLauncher.js

---
+++ src/components/ActionLauncher.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./ActionLauncher.scss";
+
+const ActionLauncher = () => {
+  return (
+    <div className="action-launcher">
+      <h3>Action Launcher</h3>
+      <button>Add Action</button>
+      <button>Fee Reversal</button>
+    </div>
+  );
+};
+
+export default ActionLauncher;
