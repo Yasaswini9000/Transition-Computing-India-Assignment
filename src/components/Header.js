6dce3fd467a8d73524e579eede31b73ab143e797
Yasaswini9000 <161327553+Yasaswini9000@users.noreply.github.com>
2024-12-01 11:01:54 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,10 @@ import "./Header.scss";
 const Header = () => {
   return (
     <header className="header">
-      <h1>My React Task</h1>
+      <h1>Chat V2 - Agent View</h1>
+      <div className="user-info">
+        <span>Spotlight on Shamim Akhtar Ansari</span>
+      </div>
     </header>
   );
 };
