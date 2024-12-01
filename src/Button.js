75b3c90818af583f881c500b3f45178878d91ea4
Yasaswini9000 <161327553+Yasaswini9000@users.noreply.github.com>
2024-12-01 11:12:13 +0530

    Update

--- src/Button.js
+++ src/Button.js
@@ -1,12 +1,1 @@
-import React from "react";
-import "./Button.scss";
 
-const Button = ({ label, onClick }) => {
-  return (
-    <button className="custom-button" onClick={onClick}>
-      {label}
-    </button>
-  );
-};
-
-export default Button;
