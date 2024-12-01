9e70d77a13cad30d8cb3dbea8228a3622515e75b
Yasaswini9000 <161327553+Yasaswini9000@users.noreply.github.com>
2024-12-01 10:36:52 +0530

    Create Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Footer.scss";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>&copy; 2024 React Task</p>
+    </footer>
+  );
+};
+
+export default Footer;
